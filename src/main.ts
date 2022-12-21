167f3916b673fff8ca9fbaed5ff9eb56ecac22c1
tuhoang <tu.hoang@gosmartlog.com>
2022-12-21 15:41:49 +0700

    update config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { enableProdMode } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { AppConfig } from './app/shared/config/config.model';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 (async () => {
-  const config = (await fetch('assets/config/app.config.json').then((res) =>
+  const config = (await fetch(`assets/config/app-config.${environment.production ? 'prod' : 'dev'}.json`).then((res) =>
     res.json(),
   )) as AppConfig;
   void AppComponent.bootstrap(config);
