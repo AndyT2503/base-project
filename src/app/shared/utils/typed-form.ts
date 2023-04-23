ca092a6d2996deb55024f11d05cf2dc949357a42
tuhoang <htu25399@gmail.com>
2023-04-23 13:22:30 +0700

    update typeForm utility

--- src/app/shared/utils/typed-form.ts
+++ src/app/shared/utils/typed-form.ts
@@ -1,15 +1,40 @@
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormArray,
+  FormControl,
+  FormGroup
+} from '@angular/forms';
 
-export type TypedFormGroup<TFormData extends object> = FormGroup<{
-  [TFormKey in keyof TFormData]: TFormData[TFormKey] extends Date
-    ? FormControl<Date>
-    : TFormData[TFormKey] extends Array<infer TItem>
-    ? TItem extends Date
-      ? FormControl<Date[]>
-      : TItem extends object
-      ? FormArray<TypedFormGroup<TItem>>
-      : FormControl<TItem[]>
-    : TFormData[TFormKey] extends object
-    ? TypedFormGroup<TFormData[TFormKey]>
-    : FormControl<TFormData[TFormKey]>;
-}>;
+type NgForm = FormArray | FormGroup | FormControl;
+
+export type TypedFormGroup<
+  TFormData extends object,
+  TOmit = object,
+> = TOmit extends Partial<Record<keyof TFormData, NgForm>>
+  ? FormGroup<{
+      [TFormKey in keyof TFormData]: TOmit[TFormKey] extends NgForm
+        ? TOmit[TFormKey]
+        : TFormData[TFormKey] extends Date
+        ? FormControl<Date>
+        : TFormData[TFormKey] extends Array<infer TItem>
+        ? TItem extends Date
+          ? FormControl<Date[]>
+          : TItem extends object
+          ? FormArray<TypedFormGroup<TItem>>
+          : FormControl<TItem[]>
+        : TFormData[TFormKey] extends object
+        ? TypedFormGroup<TFormData[TFormKey]>
+        : FormControl<TFormData[TFormKey]>;
+    }>
+  : FormGroup<{
+      [TFormKey in keyof TFormData]: TFormData[TFormKey] extends Date
+        ? FormControl<Date>
+        : TFormData[TFormKey] extends Array<infer TItem>
+        ? TItem extends Date
+          ? FormControl<Date[]>
+          : TItem extends object
+          ? FormArray<TypedFormGroup<TItem>>
+          : FormControl<TItem[]>
+        : TFormData[TFormKey] extends object
+        ? TypedFormGroup<TFormData[TFormKey]>
+        : FormControl<TFormData[TFormKey]>;
+    }>;
