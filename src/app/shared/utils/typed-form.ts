156ef1f53a62cca42db9636ceaf286905ff96070
tuhoang <htu25399@gmail.com>
2023-04-23 21:51:26 +0700

    refactor TypedForm

--- src/app/shared/utils/typed-form.ts
+++ src/app/shared/utils/typed-form.ts
@@ -5,35 +5,27 @@ import {
   FormGroup,
 } from '@angular/forms';
 
+type StandardTypedForm<TForm> = TForm extends Date
+  ? FormControl<Date>
+  : TForm extends Array<infer TItem>
+  ? TItem extends Date
+    ? FormControl<Date[]>
+    : TItem extends object
+    ? FormArray<TypedFormGroup<TItem>>
+    : FormControl<TItem[]>
+  : TForm extends object
+  ? TypedFormGroup<TForm>
+  : FormControl<TForm>;
+
 export type TypedFormGroup<
   TFormData extends object,
-  TException = object,
-> = TException extends Partial<Record<keyof TFormData, AbstractControl>>
+  TExceptionForm extends Partial<Record<keyof TFormData, AbstractControl>> = object,
+> = TExceptionForm extends Partial<Record<keyof TFormData, AbstractControl>>
   ? FormGroup<{
-      [TFormKey in keyof TFormData]: TException[TFormKey] extends AbstractControl
-        ? TException[TFormKey]
-        : TFormData[TFormKey] extends Date
-        ? FormControl<Date>
-        : TFormData[TFormKey] extends Array<infer TItem>
-        ? TItem extends Date
-          ? FormControl<Date[]>
-          : TItem extends object
-          ? FormArray<TypedFormGroup<TItem>>
-          : FormControl<TItem[]>
-        : TFormData[TFormKey] extends object
-        ? TypedFormGroup<TFormData[TFormKey]>
-        : FormControl<TFormData[TFormKey]>;
+      [TFormKey in keyof TFormData]: TExceptionForm[TFormKey] extends AbstractControl
+        ? TExceptionForm[TFormKey]
+        : StandardTypedForm<TFormData[TFormKey]>;
     }>
   : FormGroup<{
-      [TFormKey in keyof TFormData]: TFormData[TFormKey] extends Date
-        ? FormControl<Date>
-        : TFormData[TFormKey] extends Array<infer TItem>
-        ? TItem extends Date
-          ? FormControl<Date[]>
-          : TItem extends object
-          ? FormArray<TypedFormGroup<TItem>>
-          : FormControl<TItem[]>
-        : TFormData[TFormKey] extends object
-        ? TypedFormGroup<TFormData[TFormKey]>
-        : FormControl<TFormData[TFormKey]>;
+      [TFormKey in keyof TFormData]: StandardTypedForm<TFormData[TFormKey]>;
     }>;
